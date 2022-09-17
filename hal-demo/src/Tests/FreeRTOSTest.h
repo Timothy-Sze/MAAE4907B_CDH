da7270c70662b19f29efd6781e43f32a6a6c5167
unknown <thysze@gmail.com>
2022-09-17 00:10:09 -0400

    Add existing file

---
+++ hal-demo/src/Tests/FreeRTOSTest.h
@@ -0,0 +1,13 @@
+/*
+ * FreeRTOSTest.h
+ *
+ *  Created on: Nov. 18, 2021
+ *      Author: calac
+ */
+
+#ifndef TESTS_FREERTOSTEST_H_
+#define TESTS_FREERTOSTEST_H_
+
+Boolean freeRTOSTest();
+
+#endif /* TESTS_FREERTOSTEST_H_ */
---
+++ hal-demo/src/Tests/TaskSequenceTimeTest.h
@@ -0,0 +1,13 @@
+/*
+ * TaskSequenceTimeTest.h
+ *
+ *  	Created: 2022-03-18
+ *      Author: Dante Corsi
+ */
+
+#ifndef TIMETEST_H_
+#define TIMETEST_H_
+
+Boolean TaskSequenceTimeTest();
+
+#endif /* TIMETEST_H_ */
